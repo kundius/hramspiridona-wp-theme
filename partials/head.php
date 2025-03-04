d27a9fcf614efed173e2855b09ec4f99ff51db19
Kundius Ruslan <kundius.ruslan@gmail.com>
2025-03-05 01:46:27 +0300

    Update head.php

--- partials/head.php
+++ partials/head.php
@@ -2,7 +2,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset');?>" />
-<link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/favicon.png" type="image/x-icon">
+<link rel="shortcut icon" href="<?php bloginfo('template_url') ?>/favicon.ico" type="image/x-icon">
 <meta name="yandex-verification" content="" />
 <meta name="cmsmagazine" content="ef9002bde22164e1bb8ca1b1793cf47b" />
 <meta itemprop="name" content="<?php bloginfo('blogname') ?>" />
